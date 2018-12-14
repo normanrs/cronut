0186148b30b2fe6bd2ef974e1f0202514facbbb6
normanrs <normanrs@live.com>
2018-12-14 10:33:09 -0700

    initial with some spitballing

---
+++ lib/import_data.rb
@@ -0,0 +1,11 @@
+require 'json'
+module ImportData
+
+  def import_json(path)
+    file = File.read(path)
+    raw_import = JSON.parse(file)
+    raw_import["contents"].first
+  end
+
+
+end
---
+++ test/cronut_machine_test.rb
@@ -0,0 +1,13 @@
+require './lib/import_data.rb'
+require './lib/item.rb'
+require './lib/batter.rb'
+require './lib/topping.rb'
+
+class MakeCronuts
+  include ImportData
+
+  def Start
+    
+  end
+
+end
